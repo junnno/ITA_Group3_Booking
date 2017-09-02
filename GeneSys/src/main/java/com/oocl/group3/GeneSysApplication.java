69075d648587c34af3b4d29249e6815d247e453c
FoundationPig <leviemmanuelfrias@gmail.com>
2017-09-02 19:48:15 +0800

    first commit

---
+++ GeneSys/src/main/java/com/oocl/group3/GeneSysApplication.java
@@ -0,0 +1,12 @@
+package com.oocl.group3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneSysApplication.class, args);
+	}
+}
---
+++ GeneSys/src/test/java/com/oocl/group3/GeneSysApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oocl.group3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeneSysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
