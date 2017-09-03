6149a9245a73d5e77ae07e4080f8d266fb0e46a6
Christian <castich@oocl.com>
2017-09-03 17:46:45 +0800

    create sequences

---
+++ db/schema/2/create_sequences.sql
@@ -0,0 +1,11 @@
+CREATE SEQUENCE gs_user_seq
+ START WITH     1
+ INCREMENT BY   1;
+ 
+ CREATE SEQUENCE gs_booking_seq
+ START WITH     4000000000
+ INCREMENT BY   1;
+ 
+ CREATE SEQUENCE gs_container_seq
+ START WITH     1
+ INCREMENT BY   1;
