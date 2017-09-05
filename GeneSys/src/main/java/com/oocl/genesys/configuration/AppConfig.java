769e1fa662595402e343ee8662cd7c03d2647582
FoundationPig <leviemmanuelfrias@gmail.com>
2017-09-05 13:06:09 +0800

    add commit for appconfig

--- GeneSys/src/main/java/com/oocl/genesys/configuration/AppConfig.java
+++ GeneSys/src/main/java/com/oocl/genesys/configuration/AppConfig.java
@@ -7,14 +7,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.oocl.genesys")
-public class AppConfig {
-	
+public class AppConfig extends WebMvcConfigurerAdapter {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("/**").addResourceLocations("/");
+		registry.addResourceHandler("/app/**").addResourceLocations("/app/");
+		registry.addResourceHandler("/ext/**").addResourceLocations("/ext/");
+
+	}
 	@Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
