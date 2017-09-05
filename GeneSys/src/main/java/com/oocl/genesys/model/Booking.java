0d186a52a5a9fe2570705abb2260d946a7f2a410
daluplo <lsdalupan@github,com>
2017-09-05 14:35:37 +0800

    edit dao and model of booking and container

--- GeneSys/src/main/java/com/oocl/genesys/model/Booking.java
+++ GeneSys/src/main/java/com/oocl/genesys/model/Booking.java
@@ -9,6 +9,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
