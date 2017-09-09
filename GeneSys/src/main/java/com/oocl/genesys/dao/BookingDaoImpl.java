da3e90849349f7d43aed200539e2af7073ee5a1a
Christian <castich@oocl.com>
2017-09-09 15:55:59 +0800

    cho0oke

--- GeneSys/src/main/java/com/oocl/genesys/dao/BookingDaoImpl.java
+++ GeneSys/src/main/java/com/oocl/genesys/dao/BookingDaoImpl.java
@@ -53,7 +53,7 @@ public class BookingDaoImpl extends AbstractDAO<Integer, Booking> implements Boo
 	public Booking searchBkgByBkgNum(String bkgNum) {
 		// TODO Auto-generated method stub
 		Criteria criteria = createEntityCriteria();
-        criteria.add(Restrictions.eq("bkgNum", Integer.valueOf(bkgNum)));
+        criteria.add(Restrictions.eq("bkgNum", bkgNum));
         return (Booking) criteria.uniqueResult();
 	}
 
