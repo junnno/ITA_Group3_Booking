cc3e5d5186d300c909974c295a34dc0948bc65f1
Junno Beringuela <junno.paolo.beringuela@oocl.com>
2017-09-09 16:51:31 +0800

    login

--- GeneSys/src/main/java/com/oocl/genesys/controller/LoginController.java
+++ GeneSys/src/main/java/com/oocl/genesys/controller/LoginController.java
@@ -1,10 +1,12 @@
 package com.oocl.genesys.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
+import javax.xml.ws.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
@@ -29,33 +31,34 @@ public class LoginController {
 	// TODO Implement user sessions
 	// TODO Test userDAO methods
 	@ResponseBody
-	@RequestMapping(value = { "/login" }, method = RequestMethod.GET)
-	public User login(@RequestParam(required = true) String username, @RequestParam(required = true) String password,
+	@RequestMapping(value = { "/login" }, method = RequestMethod.POST)
+	public HashMap<String, Object> login(@RequestParam(required = true) String username, @RequestParam(required = true) String password,
 			HttpSession session) {
-
+		HashMap<String, Object> response = new HashMap<String, Object>();
+		System.out.println(username + password);
 		List<User> users = userService.getAllUsers();
 		for (User user : users) {
 			if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
 
 				session.setAttribute("user", user);
-
 				System.out.print("You are now logged in as ");
-				System.out.println(user.getUsername());
-
-				return user;
+				System.out.println(user.getUsername() +" "+ session.getAttribute("user"));
+				response.put("data", user);
+				response.put("success", true);
+				return response;
 			}
 		}
 		System.out.println("Error");
-		return null;
+		return response;
 	}
 
 	@ResponseBody
 	@RequestMapping(value = { "/logout" }, method = RequestMethod.GET)
 	public String logout(HttpSession session) {
 		User user = (User) session.getAttribute("user");
-
+		System.out.println(user.getUsername() +" "+ session.getAttribute("user"));
 		session.removeAttribute("user");
-
+		System.out.println("Test : "+ session.getAttribute("user"));
 		return "Successfully logged out";
 	}
 
